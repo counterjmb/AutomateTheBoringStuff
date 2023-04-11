9bf92c7bbe25f3d88b757b15e892d0785967cdbd
jbeagle <James.Beagle@nelnet.net>
2023-04-11 17:02:21 -0500

    4/11/2023 additional note on type function

--- Datat Structres.py
+++ Datat Structres.py
@@ -9,3 +9,9 @@ allCats.append({'name': 'Bronx', 'color': 'gray', 'age': 5})
 print(allCats)
 
 ## video has example of tic tac toe board
+
+#get the datatype
+print(type(42))
+print(type('hello'))
+print(type(cat))
+print(type(allCats))
