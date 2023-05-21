8d3618a2fc50b91e784eb8022b1092a5bb8cc039
counterjmb <counterjmb@gmail.com>
2023-05-20 21:55:12 -0500

    May 20th 2023

--- phoneAndEmail.py
+++ phoneAndEmail.py
@@ -3,18 +3,45 @@
 import re, pyperclip
 
 
-#TODO: Create phonenumber regex
-re.compile(r'''
-
-
-
+#phonenumber regex in varying formats
+phoneRegex = re.compile(r'''
+#415-555-0000, 555-0000, (415) 555-0000
+(                               # make everything one large group for print
+((\d\d\d)|(\(\d\d\d\)))?        # area code (optional)
+(\s|-)                          # first seperator
+\d\d\d                          # first 3 digits
+-                               # seperator
+\d\d\d\d                        # last 4 digits
+(
+    ((ext(\.)?\s) |x )          # word ext
+(\d{d,5}))?                     # extension number (optional)
+)
 ''', re.VERBOSE)
 
 
-#TODO: Create a regex for emails
+#regex for emails
+emailRegex = re.compile(r'''
+[a-zA-Z0-9_.+]+                # first half
+@                              # @ symbol
+[a-zA-Z0-9_.+]+                # domain
+''', re.VERBOSE)
 
-#TODO: Get text from clipboard
+#Get text from clipboard
+text = pyperclip.paste()
 
-#TODO: Extract the emails and phone numbers from this text
+#Extract the emails and phone numbers from this text
+extractedPhone = phoneRegex.findall(text)
+extractedEmail = emailRegex.findall(text)
 
-#TODO: Copy the extracted email/phone to the clipboard
+print(extractedPhone)
+print(extractedEmail)
+
+#first group in phone number has whole value, the each group. we just need the first one
+allPhoneNumbers = []
+for phoneNumber in extractedPhone: 
+    allPhoneNumbers.append(phoneNumber[0])
+
+#Copy the extracted email/phone to the clipboard
+results = '\n'.join(allPhoneNumbers) + '\n' + '\n'.join(extractedEmail)
+print(results)
+#pyperclip.copy(results)
