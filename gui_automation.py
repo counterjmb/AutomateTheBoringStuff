da4ec9589d82dffbe0277a597507da2f111197f6
counterjmb <counterjmb@gmail.com>
2023-05-29 22:19:55 -0500

    May 29, 2023

--- gui_automation.py
+++ gui_automation.py
@@ -19,3 +19,26 @@ pyautogui.click(100,100)
 #start python
 #import pyautogui
 #pyautogui.displayMousePosition()
+
+
+##keyboard functions
+#pyautogui.click(636,982)
+#interval for delay between each key press
+#pyautogui.typewrite('Hello Wold', interval=0.2)
+#pyautogui.typewrite(['left', 'left', 'r'])
+#get all keys
+#print(pyautogui.KEYBOARD_KEYS)
+#pyautogui.press('f1')
+#hotkeys
+#pyautogui.hotkey('ctrl', 'o')
+
+
+#screen shots and image recognition
+#create pillow image object
+#more pillow methods: https://automatetheboringstuff.com/chapter17
+pyautogui.screenshot('screenshot.png')
+#locate in screenshot another image
+#retuns x, y, width, hieght
+print(pyautogui.locateOnScreen('windows_button.png'))
+#just coordinates
+print(pyautogui.locateCenterOnScreen('windows_button.png'))
